0b7823c0493be1078d7b2e6ae887527ee84126b1
SaqibHussain786 <58483134+SaqibHussain786@users.noreply.github.com>
2019-12-03 22:38:25 +0500

    Add files via upload

---
+++ Euclidean Algo/Euclidean Algo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euclidean_Algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            Console.Write("Enter first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("GCD of {0} and {1} is equal to {2}", number1, number2, gcd(number1, number2));
+        }
+        static int gcd(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ Fast Modular Exponentiation/Fast Modular Exponentiation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast_Modular_Exponentiation
+{
+    class Program
+    {
+        static int power(int x, int y, int p)
+        {
+            
+            int res = 1;
+
+            
+            x = x % p;
+
+            while (y > 0)
+            {
+          
+                if ((y & 1) == 1)
+                    res = (res * x) % p;
+
+            
+                y = y >> 1;
+                x = (x * x) % p;
+            }
+            return res;
+        }
+
+        public static void Main()
+        {
+            int x = 2;
+            int y = 5;
+            int p = 13;
+            System.Console.WriteLine("Power is " +
+                                      power(x, y, p));
+        }
+    }
+
+
+}
+
